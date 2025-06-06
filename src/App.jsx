55a7cb6317509b2ec99abfb2471a60fec7cd0ecd
Apologle <otl5994@naver.com>
2025-06-07 02:38:39 +0900

    이때까지 한것+최신 성주님 main 병합

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ToastContainer from '@components/Toast';
+import AnswerCluster from '@pages/AnswerPage/AnswerCluster';
 import FeedPage from '@pages/FeedPage/FeedPage';
 import UserList from '@pages/list/UserList';
 import { Routes, Route } from 'react-router-dom';
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/post/:id' element={<FeedPage />} />
         <Route path='/list' element={<UserList />} />
+        <Route path='/post/answer' element={<AnswerCluster />} />
       </Routes>
       <ToastContainer />
     </>
